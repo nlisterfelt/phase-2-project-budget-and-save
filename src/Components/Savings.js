4590a479f44c0a2defe28fe786dd054dd189c0fe
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-20 17:37:46 -0500

    alphabetize goals

--- src/Components/Savings.js
+++ src/Components/Savings.js
@@ -2,7 +2,8 @@ import React from "react";
 import SavingsCard from "./SavingsCard";
 
 function Savings({goals, onSavingsDelete, onSavingsEdit}){
-    const savingsGoal = goals.map(goal=><SavingsCard key={goal.id} goal={goal} onSavingsDelete={onSavingsDelete} onSavingsEdit={onSavingsEdit}/>)
+    const savingsGoal = goals.sort((a,b)=> a.description.toLowerCase()>=b.description.toLowerCase() ? 1 : -1)
+        .map(goal=><SavingsCard key={goal.id} goal={goal} onSavingsDelete={onSavingsDelete} onSavingsEdit={onSavingsEdit}/>)
     return (
         <div>
             <h2>Savings Goals</h2>
